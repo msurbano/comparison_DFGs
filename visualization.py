0443ddb84445a07ebe10dee3184f0fd387fc9b2f
María Salas Urbano <92515344+msurbano@users.noreply.github.com>
2025-11-04 18:51:10 +0100

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -10,8 +10,10 @@ def search_differences(keys):
     df = st.session_state.original
     
     col11, col12 = col1.columns(2)
-    search = col11.selectbox('Search for', ('Existence of activities', 'Stable parts'))
+    search = col11.selectbox('Search for', ('Existence of activities', 'Identify control-flow differences'))
                             # 'Difference in frequency','Other'))
+    if(search=='Identify control-flow differences):
+       search='Stable parts'
     
     explanations = {
                 'Existence of activities': "Highlight a set of activities that are fully ('All included') or partially ('Some included') included in the DFGs.",
@@ -139,3 +141,4 @@ def filter_events(dic, act):
     
 
     
+
